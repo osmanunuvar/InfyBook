a7b7e318e65ab91c068687f841e04a06461ab40f
Osman Ünüvar <osmanunuvar@gmail.com>
2022-07-26 23:10:20 +0300

    Category Table Added

---
+++ InfyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using InfyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InfyBookWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+        public DbSet<Category> categories { get; set; }
+    }
+}

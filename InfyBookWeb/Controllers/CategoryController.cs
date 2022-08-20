33c0bad84929085b54aed6f9d409439b16d5da04
Osman Ünüvar <osmanunuvar@gmail.com>
2022-08-20 21:48:42 +0300

    categories listed

--- InfyBookWeb/Controllers/CategoryController.cs
+++ InfyBookWeb/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InfyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace InfyBookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            var objCategoryList = _db.Categories.ToList();
             return View();
         }
     }

1ab878571b15aa6400f588aeec9385c02d2b9536
Osman Ünüvar <osmanunuvar@gmail.com>
2022-08-24 23:20:04 +0300

    Server side validation (create category)

--- InfyBookWeb/Controllers/CategoryController.cs
+++ InfyBookWeb/Controllers/CategoryController.cs
@@ -27,9 +27,14 @@ namespace InfyBookWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }

4fd8b961ae42c245316e5d57c23a1c43af4d96a7
Osman Ünüvar <osmanunuvar@gmail.com>
2022-08-29 23:13:11 +0300

    Category Repository added

---
+++ InfyBook.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using InfyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfyBook.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+        void Save();
+    }
+}

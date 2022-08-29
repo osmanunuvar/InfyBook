f9f5a76300a87341c01455269e034a0d6f7fbe44
Osman Ünüvar <osmanunuvar@gmail.com>
2022-08-30 00:17:13 +0300

    implementing unit of work

--- InfyBook.DataAccess/Repository/UnitOfWork.cs
+++ InfyBook.DataAccess/Repository/UnitOfWork.cs
@@ -1,6 +1,5 @@
 ﻿using InfyBook.Data;
 using InfyBook.DataAccess.Repository.IRepository;
-using InfyBook.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,19 +8,19 @@ using System.Threading.Tasks;
 
 namespace InfyBook.DataAccess.Repository
 {
-    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    public class UnitOfWork : IUnitOfWork
     {
         private ApplicationDbContext _db;
-        public CategoryRepository(ApplicationDbContext db) : base(db)
+        public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
+            Category = new CategoryRepository(_db);
         }
+        public ICategoryRepository Category { get; private set; }
 
-        
-
-        public void Update(Category obj)
+        public void Save()
         {
-            _db.Categories.Update(obj);
+            _db.SaveChanges();
         }
     }
 }

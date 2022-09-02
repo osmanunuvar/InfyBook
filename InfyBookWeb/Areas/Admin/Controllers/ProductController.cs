b0d40c86471dde6cb9b5dfe1b6dc32247d0b921d
Osman Ünüvar <osmanunuvar@gmail.com>
2022-09-03 02:06:59 +0300

    ViewModels added

--- InfyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ InfyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using InfyBook.Data;
 using Microsoft.AspNetCore.Mvc;
 using InfyBook.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using InfyBook.Models.ViewModels;
 
 namespace InfyBookWeb.Areas.Admin.Controllers
 {
@@ -23,27 +24,26 @@ namespace InfyBookWeb.Areas.Admin.Controllers
         //GET
         public IActionResult Upsert(int? id)
         {
-            Product product = new();
-            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
-                u=> new SelectListItem
+            ProductVM productVM = new()
+            {
+                Product = new(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
                 {
-                    Text = u.Name,
-                    Value = u.Id.ToString(),
-
-                });
-            IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverType.GetAll().Select(
-                u => new SelectListItem
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
                 {
-                    Text = u.Name,
-                    Value = u.Id.ToString(),
-
-                });
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+            };
             if (id == null || id == 0)
             {
                 //create product
-                ViewBag.CategoryList = CategoryList;
-                ViewData["CoverTypeList"] = CoverTypeList;
-                return View(product);
+                //ViewBag.CategoryList = CategoryList;
+                //ViewData["CoverTypeList"] = CoverTypeList;
+                return View(productVM);
             }
             else
             {
@@ -51,7 +51,7 @@ namespace InfyBookWeb.Areas.Admin.Controllers
             }
             
             
-            return View(product);
+            return View(productVM);
         }
         //POST
         [HttpPost]
